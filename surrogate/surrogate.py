2971befb38da6afa5f7c518a3576fe99a95200f0
Shreshth Tuli <shreshthtuli@gmail.com>
2022-03-01 15:53:50 +0000

    Bug fixes

--- surrogate/surrogate.py
+++ surrogate/surrogate.py
@@ -9,23 +9,6 @@ DSET_PATH = 'data/sim_surrogate/'
 REAL_PATH = 'data/real/'
 NUM_EPOCHS = 10
 
-def train_scheduler():
-    self.feats = HOSTS
-    self.model, self.optimizer, self.scheduler, epoch, self.loss_list = load_model(self.model_name, self.feats)
-    # Load dataset
-    train_cpu, test_cpu = load_cpu_dataset(self.feats)
-    train_scheduler, test_scheduler = load_scheduler_dataset(self.feats)
-    train_energy, test_energy, self.emax = load_energy_dataset(self.feats)
-    # Train model 
-    if epoch == -1:
-        for e in tqdm(list(range(epoch+1, epoch+num_epochs+1))):
-            lossT, lr = backprop(e, self.model, self.optimizer, self.scheduler, train_cpu, train_scheduler, train_energy)
-            lossTest, _ = backprop(e, self.model, self.optimizer, self.scheduler, test_cpu, test_scheduler, train_energy, False)
-            self.loss_list.append((lossT, lossTest, lr))
-            tqdm.write(f'Epoch {e},\tTrain Loss = {lossT},\tTest loss = {lossTest}')
-            plot_accuracies(self.loss_list, base_url, self.model)
-        save_model(self.model, self.optimizer, self.scheduler, e, self.loss_list)
-
 def freeze(model):
 	for name, p in model.named_parameters():
 		p.requires_grad = False
